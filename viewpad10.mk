a947ff6d8153b94f19f9849c058522373527e86d
Funky Android Ltd <al@funkyandroid.com>
2010-12-10 09:38:46 +0000

    specify sdcard in cmdline instead of using vold.fstab

--- viewpad10.mk
+++ viewpad10.mk
@@ -12,5 +12,4 @@ PRODUCT_COPY_FILES += \
 	device/viewsonic/viewpad10/asound.conf:system/etc/asound.conf \
 	device/viewsonic/viewpad10/init.viewpad10.sh:system/etc/init.viewpad10.sh\
         device/viewsonic/viewpad10/modprobe.conf:system/etc/modprobe.conf \
-	device/viewsonic/viewpad10/vold.fstab:system/etc/vold.fstab \
 	device/viewsonic/viewpad10/Front_Center.wav:system/etc/Front_Center.wav
