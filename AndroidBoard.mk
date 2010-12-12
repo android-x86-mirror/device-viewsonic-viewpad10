66fb04fb4cb9b0a805d9e88faa91d3f67b934f6b
Funky Android Ltd <al@funkyandroid.com>
2010-12-12 08:50:04 +0000

    Removed unused keymaps setting

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_KEYMAPS_DIR := $(LOCAL_PATH)/keymaps
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/viewpad10_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/viewpad10_defconfig
