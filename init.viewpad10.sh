37251316b72509882510b63935e6a1cb4fbf8b66
Funky Android Ltd <al@funkyandroid.com>
2010-12-12 08:43:55 +0000

    Removed unneeded modprobes

--- init.viewpad10.sh
+++ init.viewpad10.sh
@@ -3,9 +3,6 @@
 chown system system  /sys/class/backlight/acpi_video0/brightness
 
 depmod
-modprobe ath3k
-modprobe rfcomm
-modprobe btusb
 modprobe ath9k
 
 # alsa_ctl -f /system/etc/asound.state restore 
