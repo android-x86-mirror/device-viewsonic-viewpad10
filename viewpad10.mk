80c54613bed1e18a86d90c68ceea5b4e8352b7c8
Funky Android Ltd <support@funkyandroid.com>
2010-12-29 15:59:18 +0000

    Updated to use asound.conf for ALSA configuration

--- viewpad10.mk
+++ viewpad10.mk
@@ -7,7 +7,8 @@ PRODUCT_DEVICE := viewpad10
 PRODUCT_MANUFACTURER := viewsonic
 PRODUCT_PACKAGE_OVERLAYS := device/viewsonic/viewpad10/overlays
 
+USE_SQUASHFS=1
+
 PRODUCT_COPY_FILES += \
-	device/viewsonic/viewpad10/asound.state:system/etc/asound.state \
-	device/viewsonic/viewpad10/init.viewpad10.sh:system/etc/init.viewpad10.sh\
-        device/viewsonic/viewpad10/modprobe.conf:system/etc/modprobe.conf \
+	$(LOCAL_PATH)/init.viewpad10.sh:system/etc/init.viewpad10.sh\
+	$(LOCAL_PATH)/asound.conf:system/etc/asound.conf
